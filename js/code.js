4d4939830186c0dea1cc7c7fb9bf2631e8575917
Simp4Hitagi <denorauten@gmail.com>
2022-11-08 16:15:07 +0200

    Added 1st layer of grid

---
+++ js/code.js
@@ -0,0 +1,15 @@
+// numbers
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+let seven = 7;
+let eight = 8;
+let nine = 9;
+
+// operators
+
+//results/equal
+
